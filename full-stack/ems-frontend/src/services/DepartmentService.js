e35f2ee7ec38c72c671535eb3dd402eac5db828a
JingjingWu710 <111849492+JingjingWu710@users.noreply.github.com>
2025-03-23 21:42:02 +0000

    Update api url

--- full-stack/ems-frontend/src/services/DepartmentService.js
+++ full-stack/ems-frontend/src/services/DepartmentService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DEPARTMENT_REST_API_BASE_URL = 'http://localhost:8080/api/departments'
+const DEPARTMENT_REST_API_BASE_URL = 'https://jingjingwuems.azurewebsites.net/api/departments'
 
 export const getAllDepartments = () => axios.get(DEPARTMENT_REST_API_BASE_URL);
 
