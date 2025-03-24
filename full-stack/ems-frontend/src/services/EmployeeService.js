cbd00d069b9f8f0138fef65e28838e68eb65a835
JingjingWu710 <111849492+JingjingWu710@users.noreply.github.com>
2025-03-24 23:52:32 +0000

    Update EmployeeService.js url with Azure backend

--- full-stack/ems-frontend/src/services/EmployeeService.js
+++ full-stack/ems-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const REST_API_BASE_URL = 'https://jingjingwuems.azurewebsites.net/api/employees';
+const REST_API_BASE_URL = 'https://ems-backend-fggffxcxf2fuhqce.uksouth-01.azurewebsites.net/api/employees';
 
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
 
